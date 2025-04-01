a8e703da668048d7de974016546322642276d6ef
athifrz1212 <athifrahman2000@gmail.com>
2025-04-01 17:40:47 +0530

    2nd commit

---
+++ src/main/java/com/blitzcode/jenkinsdemojavasev/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.blitzcode.jenkinsdemojavasev.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
