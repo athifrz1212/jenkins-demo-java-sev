15c4f45414229744d513ba4319d52a4f5721d727
athifrz1212 <athifrahman2000@gmail.com>
2025-04-01 17:37:37 +0530

    Init commit

---
+++ src/main/java/com/blitzcode/jenkinsdemojavasev/JenkinsDemoJavaSevApplication.java
@@ -0,0 +1,13 @@
+package com.blitzcode.jenkinsdemojavasev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoJavaSevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoJavaSevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blitzcode/jenkinsdemojavasev/JenkinsDemoJavaSevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blitzcode.jenkinsdemojavasev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoJavaSevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
